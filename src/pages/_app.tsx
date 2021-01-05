22eab63bed4c89cbe499dbbb8c349f4b108c1d03
Varun S Athreya <varun.sathreya@gmail.com>
2021-01-06 01:46:59 +0530

    Added progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,28 @@
+import ProgressBar from '@badrap/bar-of-progress';
 import type { AppProps } from 'next/app';
-import 'tailwindcss/tailwind.css';
+import { Router } from 'next/router';
 import Footer from '../components/footer';
 import Navbar from '../components/navbar';
+import '../css/tailwind.css';
+
+const progress = new ProgressBar({
+    color: '#60A5FA',
+    className: 'bar-of-progress',
+    delay: 100,
+    size: 2,
+});
+
+if (typeof window !== 'undefined') {
+    progress.start();
+    progress.finish();
+}
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', () => {
+    progress.finish();
+    window.scrollTo(0, 0);
+});
+Router.events.on('routeChangeError', progress.finish);
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
     return (
